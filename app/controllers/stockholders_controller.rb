70175a7219e3107f8d9535269fecdd07a26b969a
lobZter <lobst3rd.cs01@nctu.edu.tw>
2016-02-28 19:59:05 +0000

    new stockholder

--- app/controllers/stockholders_controller.rb
+++ app/controllers/stockholders_controller.rb
@@ -1,2 +1,43 @@
 class StockholdersController < ApplicationController
+  
+  # GET /stockholders/index
+  # GET /stockholders
+  def index
+  end
+  
+  # POST /stockholders
+  def create
+  end
+  
+  # GET /stockholders/:id
+  def show
+    @stockholder = Stockholder.find(params[:id])
+  end
+  
+  # PATCH /stockholders/:id
+  def update
+    @stockholder = Stockholder.find(params[:id])
+    @stockholder.update(event_params)
+
+    redirect_to :action => :show, :id => @stockholder
+  end
+  
+  # DELETE /stockholders/:id
+  def destroy
+    @stockholder = Stockholder.find(params[:id])
+    @stockholder.destroy
+  
+    redirect_to :action => :index
+  end
+  
+  # GET /stockholders/new
+  def new
+    @stockholder = Stockholder.new
+  end
+  
+  # GET /stockholders/:id/edit
+  def edit
+    @stockholder = Stockholder.find(params[:id])
+  end
+  
 end
