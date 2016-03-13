885cffe4ae13b296d21fc13dc531283cd0b106a5
lobZter <lobst3rd.cs01@nctu.edu.tw>
2016-03-13 19:59:25 +0000

    stockholder index

--- app/helpers/stockholders_helper.rb
+++ app/helpers/stockholders_helper.rb
@@ -1,2 +1,9 @@
 module StockholdersHelper
+  def check_exist(is_exist)
+    if is_exist
+      return '<span class="glyphicon glyphicon-ok"></span>'.html_safe 
+    else
+      return '<span class="glyphicon glyphicon-remove"></span>'.html_safe 
+    end
+  end
 end
