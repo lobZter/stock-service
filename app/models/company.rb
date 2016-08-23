4fe092d1f829bc6060267f7ceb0e259a00313da5
lobZter <lobst3rd.cs01@nctu.edu.tw>
2016-08-23 03:02:50 +0000

    company readonly

--- app/models/company.rb
+++ app/models/company.rb
@@ -13,7 +13,7 @@ class Company < ActiveRecord::Base
     :currency,
     :stock_num
     
-  validate :check_capital_increase, :on => :update
+  validate :readonly_field, :on => :update
   
   scope :deleted, -> { where("is_deleted=?", true)}
   scope :not_deleted, -> { where("is_deleted=?", false)}
@@ -60,62 +60,15 @@ class Company < ActiveRecord::Base
       c[:percentage] = (c[:stock_num] / total_stock_num).round(5)
     end
     @complete_tuple.sort_by! {|a| -a[:percentage]}
-    
   end
   
   private
-  def check_capital_increase
-    if self.date_establish_changed? or self.fund_changed? or self.stock_price_changed? or self.stock_class_changed? or self.currency_changed? or self.stock_num_changed?
-      
-      @capital_increase = CapitalIncrease.where("identity_id=?", self.identity.id)
-        .where("is_first=?", true)[0]
-        
-      if !@capital_increase.stock_checked
-        
-        capital_increase.destory
-        @capital_increase = CapitalIncrease.create(
-          :identity_id => self.identity.id,
-          :stock_class => self.stock_class,
-          :date_issued => self.date_establish,
-          :fund => self.fund,
-          :currency => self.currency,
-          :stock_price => self.stock_price,
-          :stock_num => self.stock_num,
-          :remark => "起始增資",
-          :is_first => true
-        )
-        
-      else
-        
-        remaining_stock = Stock.where("company_id=?", self.id)
-          .where("stock_class=?", @capital_increase.stock_class)
-          .where("date_issued=?", @capital_increase.date_issued)
-          .where("identity_id=?", @capital_increase.identity_id)[0]
-        
-        if !remaining_stock.nil? && remaining_stock.stock_num == @capital_increase.stock_num
-          remaining_stock.destory
-          capital_increase.destory
-          @capital_increase = CapitalIncrease.create(
-            :identity_id => self.identity.id,
-            :stock_class => self.stock_class,
-            :date_issued => self.date_establish,
-            :fund => self.fund,
-            :currency => self.currency,
-            :stock_price => self.stock_price,
-            :stock_num => self.stock_num,
-            :remark => "起始增資",
-            :is_first => true
-          )
-        else  
-          self.errors.add(:date_establish, "股票已變動, 無法更動起始資金資訊") if self.date_establish_changed?
-          self.errors.add(:fund, "股票已變動, 無法更動起始資金資訊") if self.fund_changed?
-          self.errors.add(:stock_price, "股票已變動, 無法更動起始資金資訊") if self.stock_price_changed?
-          self.errors.add(:stock_class, "股票已變動, 無法更動起始資金資訊") if self.stock_class_changed?
-          self.errors.add(:currency, "股票已變動, 無法更動起始資金資訊") if self.currency_changed?
-          self.errors.add(:stock_num, "股票已變動, 無法更動起始資金資訊") if self.stock_num_changed?
-        end
-        
-      end
-    end
+  def readonly_field
+    self.errors.add(:fund, "READONLY") if self.fund_changed?
+    self.errors.add(:currency, "READONLY") if self.currency_changed?
+    self.errors.add(:stock_class, "READONLY") if self.stock_class_changed?
+    self.errors.add(:stock_price, "READONLY") if self.stock_price_changed?
+    self.errors.add(:stock_num, "READONLY") if self.stock_num_changed?
   end
+  
 end
